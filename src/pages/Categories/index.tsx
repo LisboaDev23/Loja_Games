6e827d290c60681848c5d835efbbae7c714fd9dd
Gabriel Pereira Lisboa <gabriel.pereira.lisboa@gmail.com>
2024-07-30 15:27:52 -0300

    Primeira parte do projeto inserida

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,95 @@
+import ProductsList from '../../components/ProductsList'
+import Game from '../../Models/Game'
+import resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import starWars from '../../assets/images/star_wars.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description: 'Resident Evil 4 um jogo eletrizante e aterrorizante',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description: 'Resident Evil 4 um jogo eletrizante e aterrorizante',
+    title: 'Resident Evil 4',
+    system: 'PS5',
+    infos: ['5%', 'R$ 290,00'],
+    image: resident
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description: 'Diablo um jogo eletrizante e aterrorizante',
+    title: 'Diablo 3',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description: 'Resident Evil 4 um jogo eletrizante e aterrorizante',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description: 'Diablo IV é um RPG de ação em desenvolvimento',
+    title: 'Diablo 4',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: diablo
+  },
+  {
+    id: 6,
+    category: 'RPG',
+    description: 'Zelda é um RPG de ação em desenvolvimento',
+    title: 'Zelda',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: zelda
+  },
+  {
+    id: 7,
+    category: 'RPG',
+    description: 'Star Wars é um RPG de ação em desenvolvimento',
+    title: 'Star Wars',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: starWars
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description: 'Resident Evil 4 é um RPG de ação em desenvolvimento',
+    title: 'Resident Evil 4',
+    system: 'Nintendo Switch',
+    infos: ['17/05'],
+    image: resident
+  }
+]
+
+const Categories = () => (
+  <>
+    <ProductsList games={promocoes} title="RPG" background="gray" />
+    <ProductsList games={emBreve} title="Ação" background="black" />
+    <ProductsList games={promocoes} title="Aventur" background="gray" />
+    <ProductsList games={emBreve} title="FPS" background="black" />
+  </>
+)
+
+export default Categories
